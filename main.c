ab09178612dcedf71266f3ae4c1ebb99f034ae99
Lorenzo Di Gaetano <lorenzodigaetano@yahoo.it>
2022-09-07 17:47:48 +0200

    Parametri programmabili

--- main.c
+++ main.c
@@ -6,9 +6,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "copbuilder.h"
 #include "registers.h"
 #include "labels.h"
+
+#define FILENAMES_SIZE      100
 
 void printCopperlist(UWORD* copperlist) {
 	int index = 0;
@@ -23,18 +26,53 @@ void printCopperlist(UWORD* copperlist) {
     }
 }
 
+static void printInstructions() {
+    printf("Usage: \n\n");
+    printf("copbuilder <copperlistfile> <outputfile> <labelsfile>\n");
+}
 
-int main () {
+int main (int argc, char** argv) {
     
+    char  outputfilename[FILENAMES_SIZE];
+    char  labelsfilename[FILENAMES_SIZE];
+    
+    if (argc < 2) {
+        printInstructions();
+        exit(0);
+    }
+
+    char* filename = argv[1];
+
+    // outputfilename
+    if (argc > 3) {
+        strcpy(outputfilename,argv[2]);
+    } else {
+        int len = strlen (filename);
+        strcpy(outputfilename,filename);
+        strcpy(&outputfilename[len],".bin");
+    }
+
+    if (argc == 4) {
+        strcpy(labelsfilename,argv[3]);
+    } else {
+        int len = strlen (filename);
+        strcpy(labelsfilename,filename);
+        strcpy(&labelsfilename[len],"_labels.txt");
+    }
+
+    
+
+
+
     allocLabels();
-    UWORD* copperList = compileCopperlist("copperlist.pcl");
+    UWORD* copperList = compileCopperlist(filename);
 
     printCopperlist(copperList);
     
     printLabels();
     
-    writeCopperlist("Outputcop.bin");
-    writeLabels("Labels.txt");
+    writeCopperlist(outputfilename);
+    writeLabels(labelsfilename);
 
     freeCopperlist();
     freeLabels();
